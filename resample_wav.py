ab4af9bb2a0f686a66747bd6a6a279e28ad351a6
Lautaro <lau.lvm@gmail.com>
2025-04-26 22:47:30 -0300

    resample to change the sr

---
+++ resample_wav.py
@@ -0,0 +1,28 @@
+import librosa
+import soundfile as sf
+import os
+from load_data import *
+from config import sr, original_folder_path_audio, folder_path_audio
+
+def resample_wav(input_path, output_path, target_sr=sr):
+    """
+    Resamples a WAV file to a specified sample rate.
+    """
+    try:
+        # Load the audio file using librosa
+        audio, sr = librosa.load(input_path, sr=None)
+
+        # Resample the audio to the target sample rate
+        resampled_audio = librosa.resample(audio, orig_sr=sr, target_sr=target_sr)
+
+        # Save the resampled audio as a new WAV file
+        sf.write(output_path, resampled_audio, target_sr)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+os.makedirs(folder_path_audio, exist_ok=True)
+
+for filename in female_df['audio_path']:
+    file_path = os.path.join(original_folder_path_audio, filename) + '.wav'
+    resample_wav(file_path, os.path.join(folder_path_audio, filename) + '.wav')
