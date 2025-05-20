8353b7253f8f868a465b97246f057ac76084546e
Lautaro <lau.lvm@gmail.com>
2025-05-19 22:11:21 -0300

    changes in the vocabulary

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 import numpy as np
-from jiwer import wer
+from jiwer import wer, cer
 import re
 from model import build_model
 from config import (characters, batch_size, max_target_len,
@@ -12,13 +12,13 @@ import keras
 
 # Mapping characters to integers
 char_to_num = keras.layers.StringLookup(vocabulary=["<", ">"] + characters,
-                                        oov_token="",
-                                        pad_to_max_tokens=True,
-                                        max_tokens=max_target_len)
+                                        mask_token="",
+                                        oov_token="-")
 # Mapping integers back to original characters
 num_to_char = keras.layers.StringLookup(
             vocabulary=char_to_num.get_vocabulary(),
-            oov_token="", invert=True)
+            mask_token="",
+            oov_token="-", invert=True)
 
 # A utility function to decode the output of the network
 def decode_batch_predictions(results, greedy=True, beam_width=100, top_paths=1):
@@ -26,7 +26,7 @@ def decode_batch_predictions(results, greedy=True, beam_width=100, top_paths=1):
     output_text = []
     for result in results:
         # Stop where the ">" representation char is found
-        end_token_idx = tf.where(result == 2)
+        end_token_idx = tf.where(result == 3)
         if tf.size(end_token_idx).numpy() > 0:
             first_index = end_token_idx[0][0]
             result = result[:first_index]
@@ -41,7 +41,7 @@ def load_model():
         target_maxlen=max_target_len,
         num_layers_enc=num_layers_enc,
         num_layers_dec=num_layers_dec,
-        num_classes=vocab_size + 1,
+        num_classes=vocab_size,
         learning_rate=default_learning_rate)
 
     # Load the weights from the last checkpoint
@@ -69,14 +69,16 @@ if __name__ == '__main__':
     targets = []
     for batch in validation_dataset:
         X, y = batch
-        batch_predictions = model.predict(X, target_start_token_idx=1)[:, 1:]
+        batch_predictions = model.predict(X, target_start_token_idx=2)[:, 1:]
         batch_predictions = decode_batch_predictions(batch_predictions)
         predictions.extend(batch_predictions)
         batch_y = decode_batch_predictions(y[:, 1:])
         targets.extend(batch_y)
     wer_score = wer(targets, predictions)
+    cer_score = cer(targets, predictions)
     print("-" * 100)
     print(f"Word Error Rate: {wer_score:.4f}")
+    print(f"Character Error Rate: {cer_score:.4f}")
     print("-" * 100)
     for i in np.random.randint(0, len(predictions), 5):
         print(f"Target    : {targets[i]}")
