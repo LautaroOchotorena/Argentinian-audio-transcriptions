5bd22bd861f2a4af6e71bb0ba59ac7584bc6a9c4
Lautaro <lau.lvm@gmail.com>
2025-05-25 18:55:51 -0300

    adaptted to the transformer model

--- demo/app.py
+++ demo/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import numpy as np
 import io
 import soundfile as sf
+import tensorflow as tf
 # requiere installing ffmpeg
 from pydub import AudioSegment
 import time
@@ -16,34 +17,6 @@ from extract_spectrogram import extract_spectrogram
 app = Flask(__name__)
 model = load_model()
 
-# Rescoring
-if rescoring_demo:
-    # Files needed to use the rescoring
-    kenlm_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'rescoring', 'kenlm'))
-    sys.path.append(kenlm_dir)
-    from ken_lm_model import KenlmModel
-    path = os.path.expanduser(rescoring_path)
-    # the first load is slow
-    print('Loading KenLM model')
-    # Load model trained on Spanish wikipedia
-    ken_model = KenlmModel.from_pretrained(os.path.join(path, "wikipedia"),
-                                                        "es", lower_case=True)
-    print('Loaded')
-    # if choosing beam search (greedy=False) sometimes a low beam_width
-    # could lead to a worse probabily result than using greedy=True.
-    # In the other hand, having a high beam_width
-    # could leead to be worse than a lower beam_width.
-    # So it has to be in balance
-    # source: https://discuss.huggingface.co/t/is-beam-search-always-better-than-greedy-search/2943
-    greedy = False
-    beam_width = 100
-    # It expects to use beam search with top_paths > 1 as outputs
-    top_paths = 10
-else:
-    greedy = False
-    beam_width = 100
-    top_paths = 1
-
 # Makes a prediction to load everything
 def dummy_audio():
     return np.random.randn(1000)
@@ -51,6 +24,9 @@ def dummy_audio():
 def test_basic_transcription():
     audio = dummy_audio()
     spectrogram = extract_spectrogram(audio=audio)
+    # padding for the spectrogram
+    paddings = tf.constant([[0, max_time_len], [0, 0]])
+    spectrogram = tf.pad(spectrogram, paddings, "CONSTANT")[:max_time_len, :]
     batch = np.expand_dims(spectrogram, axis=0)
     model.predict(batch, target_start_token_idx=2, target_end_token_idx=3)[:, 1:]
 
@@ -100,24 +76,18 @@ def transcribe():
     try:
         spectrogram = extract_spectrogram(audio=audio)
 
+        # padding for the spectrogram
+        paddings = tf.constant([[0, max_time_len], [0, 0]])
+        spectrogram = tf.pad(spectrogram, paddings, "CONSTANT")[:max_time_len, :]
+
         spectrogram = np.expand_dims(spectrogram, axis=0)
         batch_predictions = model.predict(spectrogram, target_start_token_idx=2, target_end_token_idx=3)[:, 1:]
-        transcriptions = decode_batch_predictions(batch_predictions)[0]
-        if rescoring_demo:
-            scores = []
-            for transcription in transcriptions:
-                score = ken_model.get_perplexity(transcription)
-                scores.append(score)
-                #print(transcription, score)
-            min_index = scores.index(min(scores))
-            final_transcription = transcriptions[min_index]
-        else:
-            final_transcription = transcriptions
+        transcriptions = decode_batch_predictions(batch_predictions, target_end_token_idx=3)[0]
 
-        print("Transcription:", final_transcription)
+        print("Transcription:", transcriptions)
         end_time = time.time()
         print(f"Transcription time: {end_time - start_time:.2f} seconds")
-        return jsonify({"transcription": final_transcription})
+        return jsonify({"transcription": transcriptions})
     except Exception as e:
         # If an error occurs, the message is shown in the response
         return jsonify({"error": f"An error occurred: {str(e)}"}), 300
